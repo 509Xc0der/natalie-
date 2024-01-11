459107c1ae8d8bf4d35fe35c2fd80687118c5353
Xariposa <90054551+509Xc0der@users.noreply.github.com>
2024-01-11 15:50:05 -0600

    added sprite

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f . . . . . . 
+    . . . . f 6 2 5 5 6 f . . . . . 
+    . . . f 6 6 6 6 1 6 6 f . . . . 
+    . . . f 6 6 6 6 6 1 6 f . . . . 
+    . . . f d f d 6 6 6 1 f . . . . 
+    . . . f d f d 6 6 6 6 f f . . . 
+    . . . f d 3 d d 6 6 6 f 6 f . . 
+    . . . . f d d d f f 6 f f . . . 
+    . . . . . f f 5 3 f 6 6 6 f . . 
+    . . . . f 5 3 3 f f f f f . . . 
+    . . . . f 3 3 f d f . . . . . . 
+    . . . . . f 3 f d f . . . . . . 
+    . . . . f 3 5 3 f d f . . . . . 
+    . . . . f f 3 3 f f . . . . . . 
+    . . . . . . f f f . . . . . . . 
+    `, SpriteKind.Player)
